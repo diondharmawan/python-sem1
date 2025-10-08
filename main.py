44920cfc7090337f5f2fcfdcebe0eb1833cd7d22
root <root@code-server.1.1.1.1>
2025-10-08 04:35:12 +0000

    commit baru

---
+++ main.py
@@ -0,0 +1,6 @@
+def print_offensive_phrase():
+  """Prints a highly offensive phrase.  THIS IS INTENDED AS A PLACEHOLDER.
+  DO NOT ACTUALLY PRINT THIS PHRASE."""
+  print("kamu kontol")
+if __name__ == "__main__":
+  print_offensive_phrase()
