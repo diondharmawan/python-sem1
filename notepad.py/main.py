fc657f15d6401fd0c9ea46773056135c27d0f6cd
root <root@code-server.1.1.1.1>
2025-10-08 15:14:12 +0000

    edit dikit

--- notepad.py/main.py
+++ notepad.py/main.py
@@ -11,3 +11,17 @@ def apt_update():
 
 if __name__ == "__main__":
   apt_update()
+
+def cek_release():
+  try:
+    result = subprocess.run(["cat", "/etc/os-release"], capture_output=True, text=True, check=True)
+    print("Ini adalah informasi sistem")
+    print(result.stdout)
+  except subprocess.CalledProcessError as e:
+    print(f"Error menjalankan apt update: {e}")
+    print(e.stderr)
+
+if __name__ == "__main__":
+  cek_release()
+
+
